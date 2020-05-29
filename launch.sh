fe8ed3e57fbc876f85feba7a23a2a89d7574d609
Alan Boris <39385685+alanb128@users.noreply.github.com>
2020-05-29 16:26:23 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,37 +1,7 @@
 #!/bin/bash
 
 # Modify config.json based on config vars
-if [[ -z $VIDEO_INPUT ]]; then
-  echo 'Video input not specified - defaulting to usbcam'
-  sed -i 's/zz1/usbcam/' config.json
-else
-  echo 'Video input set to '$VIDEO_INPUT
-  sed -i "s/zz1/$VIDEO_INPUT/" config.json
-fi
-
-if [[ -z $CAM_DEV ]]; then
-  echo 'Camera not specified - defaulting to video0'
-  sed -i 's/zz2/video0/' config.json
-else
-  echo 'Camera name set to '$CAM_DEV
-  sed -i "s;zz2;$CAM_DEV;" config.json
-fi
-
-if [[ -z $CAM_IP ]]; then
-  echo 'Camera IP not specified - defaulting to none'
-  sed -i 's/zz3/none/' config.json
-else
-  echo 'Camera IP set to '$CAM_IP
-  sed -i "s;zz3;$CAM_IP;" config.json
-fi
-
-if [[ -z $VIDEO_FILE ]]; then
-  echo 'Video file not specified - defaulting to /data/demo.mp4'
-  sed -i 's|zz4|\/data\/demo.mp4|' config.json
-else
-  echo 'Video file set to '$VIDEO_FILE
-  sed -i "s;zz4;$VIDEO_FILE;" config.json
-fi
+# returning soon!
 
 # Change local URLs to container name
 sed -i "s;parsedUrl\[0\];'opendatacam';" server/utils/urlHelper.js
